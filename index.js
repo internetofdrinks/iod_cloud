6632166032741a444cbb8970d0af5fda76ac1531
Sascha Sambale <sascha.sambale@gmail.com>
2016-11-04 21:47:18 +0100

    trying stuff.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TestRoute = require('./service/routes/testRoute');
-const RestService = require('./service/RESTService');
+const RestService = require('./service/restService');
 const routes = [TestRoute.getRoute()];
 
 // start REST service
