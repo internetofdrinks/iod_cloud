151b766d92b6cfd02f5c6fc2734309c19003d6be
Sascha Sambale <sascha.sambale@gmail.com>
2016-11-05 00:15:58 +0100

    Added blood rest point! :)

--- service/routes/testRoute.js
+++ service/routes/testRoute.js
@@ -1,12 +1,27 @@
 const Route = require('./route');
 const logger = require('../utils/logger');
+const MongoDBUtils = require('../utils/mongodbutils')
 
-const REST_METHOD = 'GET';
-const REST_PATH = '/test';
 
+const REST_METHOD = 'POST';
+const REST_PATH = '/blood';
+const DB_COLLECTION = 'blood';
+const DB_MODEL = {
+  userid: String,
+  level: Number,
+  date: Date
+};
 
 const routeHandler = (request, reply) => {
-  reply('test worked fine - time for some beer! :)');
+  const mongoDBUtils = new MongoDBUtils(DB_COLLECTION);
+  let BloodLevelEntry = mongoDBUtils.createModel(DB_MODEL);
+  let bloodlevel = new BloodLevelEntry({userid: "1234", level: 1.105, date: Date.now()});
+  mongoDBUtils.save(bloodlevel).then(()=> {
+    reply().code(201)
+  }, (err)=> {
+    console.log(err);
+    reply(err).code(500)
+  })
 };
 
 class TestRoute extends Route {
---
+++ service/utils/mongodbutils.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const URI = process.env.MONGODB_URI || 'mongodb://internet_of_drinks:dr1nk_b33r@ds143777.mlab.com:43777/internetofdrinks';
+
+
+class MongoDBUtils {
+  
+  constructor(collection) {
+    this.collection = collection;
+  }
+  
+  createModel(model) {
+    return mongoose.model(this.collection, model);
+  }
+  
+  save(item) {
+    mongoose.connect(URI);
+    console.log('Connected TO: ' + URI);
+    return new Promise((resolve, reject) => {
+      item.save(function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+    
+  }
+}
+
+module.exports = MongoDBUtils;
