34cec1c3d8a37f0bcf0b7537e620d3b5f6d230c1
Sascha Sambale <sascha.sambale@gmail.com>
2016-11-05 01:53:59 +0100

    Added user route.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const BACRoute = require('./service/routes/bacRoute');
 const IndexRoute = require('./service/routes/indexRoute');
+const UserRoute = require('./service/routes/userRoute');
 const RestService = require('./service/restService');
 
-const routes = [BACRoute.getRoute(), IndexRoute.getRoute()];
+const routes = [BACRoute.getRoute(), IndexRoute.getRoute(), UserRoute.getRoute()];
 
 // start REST service
 const restService = new RestService(routes);
