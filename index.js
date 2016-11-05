456e83d4d9d96204232032d854969c2afd1903e9
Sascha Sambale <sascha.sambale@gmail.com>
2016-11-05 03:21:25 +0100

    Updated mongoose models.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const AddBACRoute = require('./service/routes/addBACRoute');
 const IndexRoute = require('./service/routes/indexRoute');
-const AddUserRoute = require('./service/routes/userRoute');
+const AddUserRoute = require('./service/routes/addUserRoute');
 const FrontendRoute = require('./service/routes/frontendRoute');
 const PublicRouter = require('./service/routes/publicRoute');
 const RestService = require('./service/restService');
 
-const routes = [BACRoute.getRoute(), IndexRoute.getRoute(), FrontendRoute.getRoute(),
-  PublicRouter.getRoute(), UserRoute.getRoute()];
+const routes = [AddBACRoute.getRoute(), IndexRoute.getRoute(), FrontendRoute.getRoute(),
+  PublicRouter.getRoute(), AddUserRoute.getRoute()];
 
 // start REST service
 const restService = new RestService(routes);
